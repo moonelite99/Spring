eb1eabd9715aea66d7db8e667b3c69be4cb38e15
moonelite99 <quanvip1999@gmail.com>
2019-11-12 20:57:11 +0700

    add $11

---
+++ src/main/java/$11_JPA_MySQL/UserRepository.java
@@ -0,0 +1,11 @@
+package $11_JPA_MySQL;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByAtk(int atk);
+}
